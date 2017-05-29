0930d68b08f7101060d26f6e4a11bcb86a372e52
manivd <mani_vidk7@hotmail.com>
2017-05-29 22:51:55 +0530

    Import packages and Create needed variables

--- app/src/main/java/com/example/sumal/morse_code_android_application/MainActivity.java
+++ app/src/main/java/com/example/sumal/morse_code_android_application/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.sumal.morse_code_android_application;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.os.HandlerThread;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.lang.reflect.Array;
+import java.security.Policy;
+import java.util.Timer;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button_send;
+    private Camera camera;
+    private boolean isFlashOn;
+    private boolean hasFlash;
+    private Parameters params;
+    private MediaPlayer mp;
+    private Timer timer ;
+    private EditText editText_msg ;
+    private String get_text ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
